2ae99c16548d79f53a5fa307e4ee5b17c334005e
sakegr <egrisafa@gmail.com>
2022-06-10 23:40:12 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', () => { 
+
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'icecream',
+            img: 'images/icecream.png'
+        },
+        {
+            name: 'icecream',
+            img: 'images/icecream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        }
+    ]
+
+    const grid = document.querySelector('.grid')
+    let cardsChosen = []
+
+    //create your board...
+    function createBoard() {
+        for(let i = 0; i < cardArray.length; i++){
+            let card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+//check for matches
+
+//flip your card
+function flipCard(){
+    let cardId = this.getAttribute('data-id')
+    cardsChosen 
+}
+
+
+
+
+
+createBoard();
+
+
+
+
+
+
+
+
+
+});
